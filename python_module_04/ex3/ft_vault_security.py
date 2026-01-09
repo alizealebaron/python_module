8b8165d96ef6d44f0a119d2968460c0e3355390d
alizealebaron <alizea.lebaron2@gmail.com>
2026-01-09 18:03:15 +0100

    Ajout 04_03

---
+++ python_module_04/ex3/ft_vault_security.py
@@ -0,0 +1,11 @@
+"""
+Fichier : ft_stream_management.py
+Auteur  : alebaron
+Date    : 2026/01/09
+"""
+
+
+if __name__ == "__main__":
+	print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+
+	print("Initiating secure vault access...")
