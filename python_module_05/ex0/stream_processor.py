22b813a1a358e08d571529574b734d01fdf2ad53
alebaron <alizea.lebaron2@gmail.com>
2026-01-10 17:21:45 +0100

    Ajout 05_00

---
+++ python_module_05/ex0/stream_processor.py
@@ -0,0 +1,15 @@
+"""
+Fichier : stream_processor.py
+Auteur  : alebaron
+Date    : 2026/01/10
+"""
+
+
+# +----------------------------------------------------------------+
+# |                         Importations                           |
+# +----------------------------------------------------------------+
+
+from typing import Any
+from abc import ABC
+
+
