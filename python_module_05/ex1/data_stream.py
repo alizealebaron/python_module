258c0976299d7eeb00d042fd75067d86c54e74f4
alebaron <alizea.lebaron2@gmail.com>
2026-01-12 18:01:34 +0100

    Ajout 05_01

--- python_module_05/ex1/data_stream.py
+++ python_module_05/ex1/data_stream.py
@@ -9,10 +9,36 @@ Date    : 2026/01/12
 # |                         Importations                           |
 # +----------------------------------------------------------------+
 
-from typing import Any, List
+from typing import Any, List, Dict, Union, Optional
 from abc import ABC, abstractmethod
 
 
 # +----------------------------------------------------------------+
 # |                       Classe abstraite                         |
 # +----------------------------------------------------------------+
+
+class StreamBase(ABC):
+
+	# +------------------------------------------------------------+
+    # |                          Méthodes                          |
+    # +------------------------------------------------------------+
+
+	@abstractmethod
+	def process_batch(self, data_batch: List[Any]) -> str:
+		pass
+
+	@abstractmethod
+	def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
+		pass
+
+	@abstractmethod
+	def get_stats(self) -> Dict[str, Union[str, int, float]]:
+		pass
+
+# +----------------------------------------------------------------+
+# |                             Main                               |
+# +----------------------------------------------------------------+
+
+if __name__ == "__main__":
+	print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
+
